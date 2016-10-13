878c3d692b7781042ca5046ea088097063d976e3
mbgazi <kimmiskim@hotmail.com>
2016-10-13 21:36:40 +0300

    deleted unnecesseray files

--- algorithm.py
+++ algorithm.py
@@ -1,13 +1,7 @@
-#from bisect import bisect_left, bisect_right 
 from array import array
 
 def values_between(input_array, a, b):
-	#range(a,b) 
-	#start = bisect_right(input_array,a)
-	#end = bisect_left (input_array,b)
-	#for i in input_array:
-	#	print i
-	#	total = total+i
+
 	out_array = []
 	if (a == b): return -1
         if (a>b): 
@@ -16,9 +10,7 @@ def values_between(input_array, a, b):
 	for i in input_array:
 		if (int(i) >int(a)  and int(i) < int( b)):
 			out_array.append(i)
-			print 'appending to array'
-                else:
+	        else:
 			exit
-	print 'result is' + str(out_array)
 	return (out_array)
 
